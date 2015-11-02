f46de6a42a912541eff66ffa00c6a9bc5513f1cf
Jan-Raphael Chua <aeroheart.c6@gmail.com>
2015-11-03 00:13:31 +0800

    initial commit for listview project

---
+++ listview/src/main/java/ph/ingenuity/listview/activity/LauncherActivity.java
@@ -0,0 +1,24 @@
+package ph.ingenuity.listview.activity;
+
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import ph.ingenuity.listview.R;
+
+
+/**
+ * @author aeroheart-c6
+ * @since 11/2/15
+ */
+public class LauncherActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        super.setContentView(R.layout.screen__launcher);
+    }
+}
+
+
+
+
