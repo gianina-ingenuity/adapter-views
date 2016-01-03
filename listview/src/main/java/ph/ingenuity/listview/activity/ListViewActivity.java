fc5556bcb3e49d08b8b9711b187019418d044b17
Jan-Raphael Chua <aeroheart.c6@gmail.com>
2016-01-03 21:54:35 +0800

    item click for listview

--- listview/src/main/java/ph/ingenuity/listview/activity/ListViewActivity.java
+++ listview/src/main/java/ph/ingenuity/listview/activity/ListViewActivity.java
@@ -1,10 +1,13 @@
 package ph.ingenuity.listview.activity;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 import ph.ingenuity.listview.R;
+import ph.ingenuity.listview.fragment.ListViewFragment;
+import ph.ingenuity.listview.model.Car;
 
 
 /**
@@ -16,5 +19,31 @@ public class ListViewActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         super.setContentView(R.layout.screen__list);
+        
+        ListViewFragment fragment;
+        
+        fragment = (ListViewFragment)this.getSupportFragmentManager().findFragmentById(R.id.content__main);
+        fragment.addListener(new FragmentListener());
+    }
+    
+    /*
+     ***********************************************************************************************
+     * Inner Classes
+     ***********************************************************************************************
+     */
+    protected class FragmentListener implements ListViewFragment.Listener {
+        @Override
+        public void onCarSelected(Car car) {
+            ListViewActivity self = ListViewActivity.this;
+            Bundle extras;
+            
+            extras = new Bundle();
+            extras.putParcelable("car", car);
+            
+            self.startActivity(
+                new Intent(self, DetailActivity.class)
+                    .putExtras(extras)
+            );
+        }
     }
 }
--- listview/src/main/java/ph/ingenuity/listview/fragment/ListViewFragment.java
+++ listview/src/main/java/ph/ingenuity/listview/fragment/ListViewFragment.java
@@ -6,8 +6,10 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import ph.ingenuity.listview.R;
@@ -21,8 +23,15 @@ import ph.ingenuity.listview.util.AssetUtil;
  * @since 12/25/15
  */
 public class ListViewFragment extends Fragment {
+    protected List<Listener> listeners;
     protected List<Car> cars;
     protected ListViewAdapter carsAdapter;
+    
+    public ListViewFragment() {
+        super();
+        
+        this.listeners = new ArrayList<>();
+    }
     
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -41,5 +50,38 @@ public class ListViewFragment extends Fragment {
 
         cars = (ListView)view.findViewById(R.id.cars);
         cars.setAdapter(this.carsAdapter);
+        cars.setOnItemClickListener(new ItemClickListener());
+    }
+    
+    protected void fireSelectionEvent(Car car) {
+        for (Listener listener : this.listeners)
+            listener.onCarSelected(car);
+    }
+    
+    public void addListener(Listener listener) {
+        this.listeners.add(listener);
+    }
+    
+    /*
+     ***********************************************************************************************
+     * Inner Classes
+     ***********************************************************************************************
+     */
+    protected class ItemClickListener implements AdapterView.OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView<?> source, View view, int position, long itemId) {
+            ListViewFragment self = ListViewFragment.this;
+            
+            self.fireSelectionEvent(self.cars.get(position));
+        }
+    }
+    
+    /*
+     ***********************************************************************************************
+     * Interfaces
+     ***********************************************************************************************
+     */
+    public interface Listener {
+        void onCarSelected(Car car);
     }
 }
